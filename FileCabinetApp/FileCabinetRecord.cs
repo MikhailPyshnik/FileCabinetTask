9eea1f7fe0dc44ef92969d31efde5178ecfb1277
MikhailPyshnik <misha.pyshnik@gmail.com>
2019-10-10 23:58:50 +0300

    Add FileCabinetRecord.cs file.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class FileCabinetRecord
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+}
