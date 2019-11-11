69715d284c618ca4d3bcf25314f5325a4781fcf2
MikhailPyshnik <misha.pyshnik@gmail.com>
2019-11-12 02:03:25 +0300

    Implementation Chain of Responsibility.

---
+++ FileCabinetApp/CommandHandlers/AppCommandRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Work AppCommandRequest.
+    /// </summary>
+    public class AppCommandRequest
+    {
+        /// <summary>
+        /// Gets or sets the Command.
+        /// </summary>
+        /// <value>The Command.</value>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Parameters.
+        /// </summary>
+        /// <value>The Command.</value>
+        public string Parameters { get; set; }
+    }
+}
---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Base class CommandHandler.
+    /// </summary>
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler nextHandler;
+
+        /// <summary>
+        ///  Implementation method SetNext by ICommandHandler in CommandHandlerBase..
+        /// </summary>
+        /// <param name="commandHandler">Input parametr record <see cref="ICommandHandler"/>.</param>
+        /// <returns>Id <see cref="int"/>.</returns>
+        public ICommandHandler SetNext(ICommandHandler commandHandler)
+        {
+            this.nextHandler = commandHandler;
+            return this.nextHandler;
+        }
+
+        /// <summary>
+        /// Implementation method Handle by ICommandHandler in CommandHandlerBase.
+        /// </summary>
+        /// <param name="appCommandRequest">Input parametr record <see cref="AppCommandRequest"/>.</param>
+        public virtual void Handle(AppCommandRequest appCommandRequest)
+        {
+            if (this.nextHandler != null)
+            {
+               this.nextHandler.Handle(appCommandRequest);
+            }
+            else
+            {
+                this.nextHandler = null;
+            }
+        }
+    }
+}
